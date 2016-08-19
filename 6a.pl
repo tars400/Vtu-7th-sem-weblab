fc457b8e3e1e5e7285473111cca1c878b90978c8
Tarun S <tarun.shiva.sujatha@gmail.com>
2016-08-19 22:28:47 +0530

    Add files via upload

---
+++ 6a.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use CGI;
+$q = new CGI;
+print "Content-type:text/html\n\n";
+$cmd = $q->param("name");
+@greet=("Bye","Good day","Greetings","Aloha");
+$index=int(rand(4));
+print<<check;
+<html>
+	<head><title>Welcome</title></head>
+	<body bgcolor=blue>
+		<h1>Hi, $cmd <br/> @greet[$index].</h1>
+	</body>
+</html>
+check
