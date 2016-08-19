e9cd78b8056227baa96d59052a8b353a729c67fa
Tarun S <tarun.shiva.sujatha@gmail.com>
2016-08-19 22:30:43 +0530

    Add files via upload

---
+++ 7.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# Implementing a Digital Clock
+use CGI':standard';
+print "Refresh:1 \n"; 
+($s,$m,$h)=localtime(time);
+print "Content-type:text/html \n\n";
+print start_html(-title=>"Program 7",-bgcolor=>"pink",-text=>"black");
+print "The system time is $h:$m:$s", "<br>"; 
+print "In words $h hours $m minutes $s seconds";
+
