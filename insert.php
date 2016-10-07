1bbba2b7b716ed2a55035f03c83627a71e8f783c
Tarun S <tarun.shiva.sujatha@gmail.com>
2016-10-07 12:50:45 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,10 +6,11 @@
 
         $con=mysql_connect('localhost','root','root123') or die('Could not connect');
          mysql_select_db('test',$con);
-
-            if(mysql_query("insert into student values('$name','$addr1','$addr2','$email');")){
-                        echo "Data inserted successfully!";
-                    }
+            
+        if(mysql_query("insert into student values('$name','$addr1','$addr2','$email');"))
+            {
+                echo "Data inserted successfully!";
+            }
      
         mysql_close($con);
  ?>
